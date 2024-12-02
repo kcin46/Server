ce96bf7d1e64c9a3135f37513e2395f8c007824f
kcin46 <ajax053@yahoo.com>
2024-12-02 14:28:47 -0500

    Update common.h

--- common.h
+++ common.h
@@ -2,8 +2,8 @@
 #define _COMMON_H_
 
 #define SERVER_HOSTNAME "localhost"  /* the default server hostname */
-//#define SERVER_HOSTNAME "emesis.csic.umd.edu"
-#define MAGIC_STRING "cmsc417fall2015"
+
+#define MAGIC_STRING ""
 #define SERVER_PORT 9999  /* the default server port */
 #define MAX_STR_SIZE 255  /* maximum length of any messages */
 #define MAXPENDING 16 /* maximum number of pending clients allowed */
